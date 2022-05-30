f2b70051133a306d634009e9ba265856d04a6635
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-05-30 18:57:18 -0300

    Correções

--- front/src/pages/OnlyViewCams/OnlyViewCams.jsx
+++ front/src/pages/OnlyViewCams/OnlyViewCams.jsx
@@ -18,6 +18,7 @@ export function OnlyViewCams(props) {
     async function getInfo() {
         getRequest(`onlyaccesscam/${token}`,token).then(
             (response) => {
+                console.log(response.data)
                 if (response.data.data != null) {
                     setCams(response.data.data.map((cam, i) => (
                         <CamViewCard
