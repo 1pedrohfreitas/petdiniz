048d05774be548239533c6bb859ed4e6f43056e0
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-05-30 11:00:02 -0300

    Correção

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,17 @@ docker stop install-petdiniz_front-1
 
 echo "Removendo os containers"
 docker rm install-petdiniz_front-1
-#docker rm install-petdiniz_back-1
+docker rm install-petdiniz_back-1
 
 echo "Removendo as imagens"
 docker rmi pedrohfreitas/petdiniz_front:1.0
-#docker rmi pedrohfreitas/petdiniz_back:1.0
+docker rmi pedrohfreitas/petdiniz_back:1.0
+
+echo "Construindo a imagem do servidor de cameras"
+cd rmtp && docker build -t pedrohfreitas/petdiniz_rmtp:1.0 .
 
 echo "Construindo a imagem do front"
-cd front && docker build -t pedrohfreitas/petdiniz_front:1.0 .
+cd .. front && docker build -t pedrohfreitas/petdiniz_front:1.0 .
 
 echo "Construindo a imagem do back"
 cd .. && cd back && docker build -t pedrohfreitas/petdiniz_back:1.0 .
