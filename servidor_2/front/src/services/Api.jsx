b69d65eaeab7f31640f77fbdba7899fada8e810e
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-06-03 01:29:25 -0300

    Subindo alterações

--- servidor_2/front/src/services/Api.jsx
+++ servidor_2/front/src/services/Api.jsx
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 export function getBaseURL() {
-  return `https://api.net-apps.info/api/v1/`
+  return `https://petsdiniz-api.net-apps.info/api/v1/`
   // return `http://localhost:5000/api/v1/`
 }
 export function getStaticFilesUrl() {
