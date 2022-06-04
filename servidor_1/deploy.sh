842e440136ef30f7efbcb36780af5b0ab527897a
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-06-04 07:46:50 -0300

    Correção deploy

--- servidor_1/deploy.sh
+++ servidor_1/deploy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #echo "Parando os serviços"
-#docker stop petdiniz-petdiniz_back-1
+docker stop servidor_1-petdiniz_back-1
 
 #echo "Removendo os containers"
-#docker rm petdiniz-petdiniz_back-1
+docker rm servidor_1-petdiniz_back-1
 
 #echo "Removendo as imagens"
-#docker rmi pedrohfreitas/petdiniz_back:1.0
-
-#echo "Construindo a imagem do front"
-#cd front && docker build -t pedrohfreitas/petdiniz_front:1.0 .
+docker rmi pedrohfreitas/petdiniz_back:1.0
 
 #echo "Subindo a stack de serviços"
-#cd .. && docker compose up -d
+docker compose up -d
 
 #Criando a pasta DB caso não exista
-mkdir -p /root/db
+#mkdir -p /root/db
