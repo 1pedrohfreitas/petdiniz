5072ee086eeea7cbb75dcc8e3d13583c6957ff21
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-06-04 09:18:39 -0300

    correções

--- servidor_2/deploy.sh
+++ servidor_2/deploy.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 echo "Parando os serviços"
-docker stop petdiniz-petdiniz_front-1
-docker stop petdiniz-petdiniz_rmtp-1
+docker stop servidor_2-petsdiniz_front-1
+#docker stop petdiniz-petdiniz_rmtp-1
 
 echo "Removendo os containers"
-docker rm petdiniz-petdiniz_front-1
-docker rm petdiniz-petdiniz_rmtp-1
+docker rm servidor_2-petsdiniz_front-1 -f
+#docker rm petdiniz-petdiniz_rmtp-1
 
 echo "Removendo as imagens"
 docker rmi pedrohfreitas/petdiniz_front:1.0
-docker rmi pedrohfreitas/petdiniz_rmtp:1.0
-
-echo "Construindo a imagem do front"
-cd front && docker build -t pedrohfreitas/petdiniz_front:1.0 .
-
-echo "Construindo a imagem do rmtp"
-cd .. && cd back && docker build -t pedrohfreitas/petdiniz_rmtp:1.0 .
+#docker rmi pedrohfreitas/petdiniz_rmtp:1.0
 
 echo "Subindo a stack de serviços"
-cd .. && docker compose up -d
+cd docker compose up -d
