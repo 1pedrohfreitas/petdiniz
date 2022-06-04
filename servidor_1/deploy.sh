a6fed8163b7e3dc364cc9ea0c4b5f2b73c80bdc9
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-06-04 08:03:35 -0300

    Subindo correções

--- servidor_1/deploy.sh
+++ servidor_1/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #echo "Parando os serviços"
-docker stop servidor_1-petdiniz_back-1
+docker stop servidor_1-petdiniz_back-1 -f
 
 #echo "Removendo os containers"
-docker rm servidor_1-petdiniz_back-1
+docker rm servidor_1-petdiniz_back-1 -f
 
 #echo "Removendo as imagens"
-docker rmi pedrohfreitas/petdiniz_back:1.0
+docker rmi pedrohfreitas/petdiniz_back:1.0 -f
 
 #echo "Subindo a stack de serviços"
 docker compose up -d
